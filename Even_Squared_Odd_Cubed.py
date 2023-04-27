be021846fc5d0e22b32f09ea1bdd7628dfcfb788
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-04-27 17:05:58 +0800

    Introduction

---
+++ Even_Squared_Odd_Cubed.py
@@ -0,0 +1,5 @@
+ #BABIERA,ALEXA | CMPE-103-MODULE-2-FILE-HANDLING-IN PYTHON | PROGRAMMING EXERCISE 4
+#This program reads a text file that contains a set of numbers. 
+# Then, the even and odd numbers will be obtained from the text file. 
+# The extracted even numbers will be squared and the odd numbers will be cubed. 
+# The result will be transferred to separate files.
--- README.md
+++
@@ -1,2 +0,0 @@
-# EVEN-SQUARED-ODD-CUBED
-This program reads a text file that contains a set of numbers. Then, the even and odd numbers will be obtained from the text file. The extracted even numbers will be squared and the odd numbers will be cubed. The result will be transferred to separate files.
