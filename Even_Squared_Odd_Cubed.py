e64565b424ff4826c6c273fb4e8b8a99abdf2322
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-04-28 02:04:25 +0800

    Working Code

--- Even_Squared_Odd_Cubed.py
+++ Even_Squared_Odd_Cubed.py
@@ -4,11 +4,43 @@
 # The extracted even numbers will be squared and the odd numbers will be cubed. 
 # The result will be transferred to separate files.
 
-# import the necessary module
+#import the necessary module
 from pyfiglet import figlet_format
 import pygame
 from termcolor import colored
 import pyfiglet
 from colorama import Back, Fore, Style, init
 import time
+
+
+# OPEN file (numbers.txt,even.txt,odd.txt) as write
+with open("number.txt", 'w') as Number_File:
+  while True :
+     try:
+        # GET user input (20 numbers/Integers) and store it in numbers.txt file
+         user = int(input(Fore.LIGHTBLUE_EX + "Enter a number: " + Fore.YELLOW))
+         if user <= 0 or user >= 0:
+           Number_File.write(str(user) + "\n")
+           continue
+     except:
+         break      
+with open("number.txt", 'r') as Number_File, open("even.squared.txt", 'w') as Even_Squared, open("odd.cubed.txt", 'w') as Odd_Cubed:
+        # LOOP the lines in number.txt file
+         for line in Number_File:
+        # ASSIGN each line from number.txt as an integer variable
+          Number = int(line)
+        # CHECK if Number is EVEN
+          if Number % 2 == 0:
+            #Square the even number
+            New_Even = Number ** 2
+            # APPEND the Number to even.txt file
+            Even_Squared.write(str(New_Even) + "\n")
+
+        # CHECK if Number is ODD
+          elif Number % 2 != 0:
+            #Square the odd number
+            New_Odd = Number ** 3
+            # APPEND the Number to odd.txt file
+            Odd_Cubed.write(str(New_Odd) + "\n")
+
 
