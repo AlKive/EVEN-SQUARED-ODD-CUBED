fb52470f982f13bc57e8909292a21616efc1f156
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-04-28 00:24:56 +0800

    Importibf Modules

--- Even_Squared_Odd_Cubed.py
+++ Even_Squared_Odd_Cubed.py
@@ -1,5 +1,14 @@
- #BABIERA,ALEXA | CMPE-103-MODULE-2-FILE-HANDLING-IN PYTHON | PROGRAMMING EXERCISE 4
+# BABIERA,ALEXA | CMPE-103-MODULE-2-FILE-HANDLING-IN PYTHON | PROGRAMMING EXERCISE 4
 #This program reads a text file that contains a set of numbers. 
 # Then, the even and odd numbers will be obtained from the text file. 
 # The extracted even numbers will be squared and the odd numbers will be cubed. 
 # The result will be transferred to separate files.
+
+# import the necessary module
+from pyfiglet import figlet_format
+import pygame
+from termcolor import colored
+import pyfiglet
+from colorama import Back, Fore, Style, init
+import time
+
